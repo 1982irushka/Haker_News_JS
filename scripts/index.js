4463b4692758585d1013fce9f3d754a564fe3374
1982irushka <irushkachka@ukr.net>
2022-07-16 23:07:02 +0300

    eslint fixing

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { formUser } from './modules/form.js';
 import { News } from './modules/news.js';
 
@@ -15,55 +14,6 @@ function getAgeValidity(value) {
   const isNumber = !Number.isNaN(value) && typeof value === 'number';
   return isNumber && Number(value) > 0;
 }
-/* 
-async function getNews() {
-  const ids = await fetch(
-    'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
-  );
-  const allNewsIds = await ids.json();
-  console.log('ids', ids);
-  console.log('allNewsIds', allNewsIds);
-
-  const newsFiveIds = allNewsIds.slice(0, 5);
-  console.log('newsFiveIds', newsFiveIds);
-  const fivePromises = newsFiveIds.map((id) =>
-    fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
-  );
-  console.log('fivePromises', fivePromises);
-
-  const newsFiveData = await fivePromises.map((response) => response.json());
-
-  console.log('newsFiveData', newsFiveData);
-  
-}
-
-getNews(); */
-
-///////asynk await ver
-/* 
-inet ver
-async function getNewsComments() {
-  const ids = await (
-    await fetch(
-      'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
-    )
-  ).json();
-  const newsIdsFour = ids.slice(0, 5);
-  const newsDataFivePromises = //newsDataFour маси 5промісів
-    newsIdsFour.map(
-      async (id) =>
-        await (
-          await fetch(
-            `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
-          )
-        ).json()
-    );
-  const pyatNovin = await Promise.all(newsDataFivePromises); // 5 новин
-  console.log('pyatNovin', pyatNovin);
-
-}
-getNewsComments(); */
-/////////////////////////////////////////////THEN ver
 
 const newsAllPromise = fetch(
   'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
@@ -111,7 +61,7 @@ newsAllPromise
       const {
         by,
         descendants,
-        time,
+        time: titleTime,
         title: titleNews,
         score,
         url,
@@ -121,7 +71,7 @@ newsAllPromise
       const commentsHtml = comments
         .filter(({ id }) => kids.slice(0, 4).includes(id))
         .reduce(
-          (acc, { id, text, time }) => ` ${acc} 
+          (acc, { text, time }) => ` ${acc} 
         <li class="generic-list__item">
            <article class="generic-list__content">
              <p class="generic-list__detail">
@@ -133,18 +83,12 @@ newsAllPromise
          </li>`,
           ''
         );
-      // const { hostname = null } = url ? new URL(url) : {};
       const newsUrl = url ? new URL(url) : {};
-      //      якщо  url (змінна з деструкції newsOne) true то повертається url
-      //а якщо ні то пустий об'єкт
       const { hostname = null } = newsUrl;
-
-      //деструкція обєкта.якщлв обєкті newsUrl для проперті hostname значення немає
-      // то виводиться null
       const heading = `<h2 class="generic-list__title">${titleNews}</h2>`;
       const headingWithLink = `<a href="${url}">${heading}</a>`;
       const source = url && hostname ? `<a href="${url}">${hostname}</a>` : '';
-      // якщо url і hostname true тоді повертається <a> а якщо faulse тоді пустий рядок.
+
       return `${accom}
         <li class="generic-list__item">
           <article class="news">
@@ -157,7 +101,7 @@ newsAllPromise
             <p>
               <span>${descendants}</span> points by
               <span>${by}</span>
-              <span>${time}</span> | <!-- timestamp to readable date -->
+              <span>${titleTime}</span> | <!-- timestamp to readable date -->
               <button class="generic-list__show-comments">${score} comments</button>
             </p>
           </article>
@@ -180,34 +124,32 @@ newsAllPromise
     });
   });
 
-////////////
+const userForm = document.forms.user;
 
-let userForm = document.forms.user;
-
-userForm.onsubmit = function (event) {
+userForm.onsubmit = function validationForm(event) {
   event.preventDefault();
 
   const errors = new Map();
 
-  let countrySelect = userForm.elements.country;
+  const countrySelect = userForm.elements.country;
 
-  let countrySelectValue = countrySelect.value;
+  const countrySelectValue = countrySelect.value;
 
   if (countrySelectValue === '') {
     errors.set('country', 'Please choose country');
   } else {
-    let infoUserCountry = document.getElementById('userInfoCountry');
+    const infoUserCountry = document.getElementById('userInfoCountry');
     infoUserCountry.innerHTML = `<p>Country</p><span class="user-info__value">${countrySelectValue}</span>`;
     if (errors.has('country')) {
       errors.delete('country');
     }
   }
 
-  let genderRadio = userForm.elements.gender;
-  let genderRadioMaleChecked = genderRadio[0].checked;
-  let genderRadioFemaleChecked = genderRadio[1].checked;
+  const genderRadio = userForm.elements.gender;
+  const genderRadioMaleChecked = genderRadio[0].checked;
+  const genderRadioFemaleChecked = genderRadio[1].checked;
   let genderRadioValue;
-  let infoUserGender = document.getElementById('userInfoGender');
+  const infoUserGender = document.getElementById('userInfoGender');
 
   if (genderRadioFemaleChecked === true) {
     genderRadioValue = genderRadio[1].value;
@@ -227,10 +169,10 @@ userForm.onsubmit = function (event) {
     errors.set('gender', 'Please choose gender');
   }
 
-  let userFormAgeValue = userForm.elements.age.value;
+  const userFormAgeValue = userForm.elements.age.value;
   const isValidAge = getAgeValidity(Number(userFormAgeValue));
   if (isValidAge) {
-    let infoUserAge = document.getElementById('userInfoAge');
+    const infoUserAge = document.getElementById('userInfoAge');
     infoUserAge.innerHTML = `<p>Age</p><span class="user-info__value">${userFormAgeValue}</span>`;
     if (errors.has('age')) {
       errors.delete('age');
@@ -239,37 +181,47 @@ userForm.onsubmit = function (event) {
     errors.set('age', 'please enter number to age field');
   }
 
-  let nameText = userForm.elements.name;
-  let nameTextValue = nameText.value;
+  const nameText = userForm.elements.name;
+  const nameTextValue = nameText.value;
   const isValidName = nameTextValue && /^[a-z]+$/.test(nameTextValue);
 
   if (isValidName) {
     if (errors.has('name')) {
       errors.delete('name');
     }
-    let infoUserName = document.getElementById('userInfoName');
+    const infoUserName = document.getElementById('userInfoName');
     infoUserName.innerHTML = `<p>Name</p><span class="user-info__value">${nameTextValue}</span>`;
   } else {
     errors.set('name', 'Please fill name input');
   }
-
+  /* 
   invalidMessage(errors);
   if (errors.size === 0) {
     validField(errors);
-  }
+  } */
 
   function invalidMessage(mapOfErrors) {
-    let formMistake = document.getElementById('formMistake');
+    const formMistake = document.getElementById('formMistake');
     let errorMgs = '';
     mapOfErrors.forEach((value) => {
       errorMgs = `${errorMgs}<li class="form__mistake">${value}</li>`;
     });
     formMistake.innerHTML = errorMgs;
   }
+  /* 
+  invalidMessage(errors);
+  if (errors.size === 0) {
+    validField(errors);
+  } */
 
   function validField() {
-    let userInformation = document.getElementById('userInfo');
+    const userInformation = document.getElementById('userInfo');
     userForm.style.display = 'none';
     userInformation.style.display = 'block';
+  }
+
+  invalidMessage(errors);
+  if (errors.size === 0) {
+    validField(errors);
   }
 };
